5928ee7d07cd23f25d43afe9f97180493f53a81f
blazercloud <h7059040814@outlook.com>
2025-10-29 10:36:19 +0800

    修复启动类找不到mapper的问题

--- back-end-java/src/main/java/com/neusoft/community/CommunityApplication.java
+++ back-end-java/src/main/java/com/neusoft/community/CommunityApplication.java
@@ -1,34 +1,17 @@
-
-
 package com.neusoft.community;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-/**
- * 智慧社区平台启动类
- * 
- * @author Neusoft
- * @date 2024-10-27
- */
-@SpringBootApplication(scanBasePackages = "com.neusoft.community")
+@SpringBootApplication
+@MapperScan("com.neusoft.community.**.mapper") // ⭐ 自动扫描所有子包下的 mapper
 public class CommunityApplication {
-
-    private static final Logger logger = LoggerFactory.getLogger(CommunityApplication.class);
-
     public static void main(String[] args) {
-        try {
-            ConfigurableApplicationContext context = SpringApplication.run(CommunityApplication.class, args);
-            logger.info("\n=================================");
-            logger.info("东软智慧社区平台启动成功！");
-            logger.info("API地址: http://localhost:8080/api");
-            logger.info("=================================\n");
-        } catch (Exception e) {
-            logger.error("东软智慧社区平台启动失败", e);
-        }
+        SpringApplication.run(CommunityApplication.class, args);
+        System.out.println("=================================");
+        System.out.println("东软智慧社区平台启动成功！");
+        System.out.println("API地址: http://localhost:8989/api");
+        System.out.println("=================================");
     }
 }
